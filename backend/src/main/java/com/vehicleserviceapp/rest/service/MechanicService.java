139848c3ab633cfade8f6faeca0ec936e8df88d6
project23 <shubham8991401@gmail.com>
2023-05-11 21:08:07 +0530

    commit2

---
+++ backend/src/main/java/com/vehicleserviceapp/rest/service/MechanicService.java
@@ -0,0 +1,93 @@
+package com.vehicleserviceapp.rest.service;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import com.vehicleserviceapp.rest.entity.Customer;
+import com.vehicleserviceapp.rest.entity.Mechanic;
+import com.vehicleserviceapp.rest.entity.Request;
+import com.vehicleserviceapp.rest.entity.User;
+import com.vehicleserviceapp.rest.repository.MechanicServiceRepository;
+
+@Component
+@Service("mechanicService")
+public class MechanicService  {
+
+	@Autowired
+	@Qualifier("mechanicRepository")
+	private MechanicServiceRepository mechanicRepo;
+	
+	@Autowired
+	private RequestService requestService;
+	
+	@Autowired
+	private UserService userService;
+	
+	public List<Mechanic> getAllMechanics(){
+		List<Mechanic> getAllMechanic = mechanicRepo.findAll();
+		return getAllMechanic;
+	}
+	
+	public Mechanic getOneMechanic(Integer mechanicId) {
+		Optional<Mechanic> MechanicOpt = mechanicRepo.findById(mechanicId);
+		Mechanic foundMechanic = MechanicOpt.get();
+		return foundMechanic;
+	}
+	
+	public Mechanic createMechanic(Mechanic mechanic) {
+		User newUser=new User();
+		newUser.setEmailId(mechanic.getEmailId());
+		newUser.setPassword(mechanic.getPassword());
+		newUser.setRole("mechanic");
+		newUser=userService.createUser(newUser);
+		mechanic.setUser(newUser);
+		Mechanic createdMechanic = mechanicRepo.save(mechanic);
+		return createdMechanic;
+	}
+	
+	public void deleteOneMechanic(Integer mechanicId) {
+		
+		List<Request> list=requestService.getAllRequestBymechanic(mechanicId);
+		if(list.size()!=0) {
+		for(int i=0;i<list.size();i++)
+		{
+			requestService.updateRequestForMechanic(list.get(i).getRequestId(), list.get(i));
+		}
+		}
+		
+		mechanicRepo.deleteById(mechanicId);
+	}
+	
+	public ResponseEntity<Mechanic> updateMechanic ( Integer mechId, Mechanic mechanicDetails) {
+
+		Optional<Mechanic> mechanicOpt=mechanicRepo.findById(mechId);
+		Mechanic foundMechanic=mechanicOpt.get();
+		
+		foundMechanic.setFirstName(mechanicDetails.getFirstName());
+		foundMechanic.setLastName(mechanicDetails.getLastName());
+		foundMechanic.setEmailId(mechanicDetails.getEmailId());
+		foundMechanic.setAddress(mechanicDetails.getAddress());
+		foundMechanic.setMobileNo(mechanicDetails.getMobileNo());
+		foundMechanic.setPassword(mechanicDetails.getPassword());
+		foundMechanic.setSkills(mechanicDetails.getSkills());
+		foundMechanic.setSalary(mechanicDetails.getSalary());
+		
+		Mechanic updateMechanic = mechanicRepo.save(foundMechanic);
+		return ResponseEntity.ok(updateMechanic);
+	}
+
+	public Mechanic getMechanicByUser(User user)
+	{
+		return mechanicRepo.findByuser(user);
+	}
+
+	
+}
+
---
+++ backend/src/test/java/com/vehicleserviceapp/rest/VehicleServiceManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vehicleserviceapp.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleServiceManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
